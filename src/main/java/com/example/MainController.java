9265d5557569de4781878fbab27e34adff92633c
Szép Andor <andorbandi08@gmail.com>
2026-04-13 13:28:51 +0200

    kezdés

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.RowConstraints;
+
+public class MainController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private RowConstraints gridPane;
+
+    @FXML
+    void onClickSaveButton(ActionEvent event) {
+
+    }
+
+}
